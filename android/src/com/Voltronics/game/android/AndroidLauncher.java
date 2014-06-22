2e934677844c1557c6b7fae5d044669cb4e92b83
z-chenet <z.chenet@gmail.com>
2014-06-22 15:37:29 -0400

    libGDX Project Start

---
+++ android/src/com/Voltronics/game/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.Voltronics.game.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.Voltronics.game.LayrdGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new LayrdGame(), config);
+	}
+}
